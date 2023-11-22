8b5677d6c71b9203712deb33f5372ca532e1772d
Midas24kMidas24k <137853877+Midas24k@users.noreply.github.com>
2023-11-22 13:02:11 -0500

    progress on script.js ran into bug

---
+++ Assets/JavaScript/script.js
@@ -0,0 +1,63 @@
+var startButton = document.getElementById('start-btn')
+var nextButton = document.getElementById('next-btn')
+var questionContainerElement = document.getElementById('question-container')
+var questionElement = document.getElementById('question')
+var answerButtonsElement = document.getElementById('answer-buttons')
+let shuffledQuestions, currentQuestionIndex
+
+// this is for the start button duh
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+  console.log('start')
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    var button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  nextbutton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+
+function selectAnswer(e) {
+
+
+}
+
+var questions = [
+  {
+    question: 'What is 2 + 2',
+    answers: [
+      { text: '4', correct: true },
+      { text: '6', correct: false },
+      { text: '8', correct: false },
+      { text: '10', correct: false }
+    ]
+  }
+]
